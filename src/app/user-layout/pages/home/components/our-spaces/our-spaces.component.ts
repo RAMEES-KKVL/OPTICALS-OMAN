06cd520fdd6b607780386159d9d156a36c5b5153
RAMEES-KKVL <rameesp41750@gmail.com>
2025-07-01 01:03:03 +0530

    Completed UI design of our-apces section

---
+++ src/app/user-layout/pages/home/components/our-spaces/our-spaces.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-our-spaces',
+  imports: [],
+  templateUrl: './our-spaces.component.html',
+  styleUrl: './our-spaces.component.css'
+})
+export class OurSpacesComponent {
+
+}
--- src/app/user-layout/pages/home/home.component.ts
+++ src/app/user-layout/pages/home/home.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { BannerComponent } from './components/banner/banner.component';
+import { OurSpacesComponent } from './components/our-spaces/our-spaces.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    // BannerComponent,
+    OurSpacesComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
