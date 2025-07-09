4f9c14d61c5405a5da455072f827a4b17afb69c8
RAMEES-KKVL <rameesp41750@gmail.com>
2025-07-09 18:40:49 +0530

    Completed UI design of featured products

--- src/app/user-layout/pages/home/home.component.ts
+++ src/app/user-layout/pages/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from './components/banner/banner.component';
 import { OurSpacesComponent } from './components/our-spaces/our-spaces.component';
+import { FeaturedProductsComponent } from './components/featured-products/featured-products.component';
 
 @Component({
   selector: 'app-home',
   imports: [
-    // BannerComponent,
-    OurSpacesComponent
+    BannerComponent,
+    OurSpacesComponent,
+    FeaturedProductsComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
