f2134bc781cf4952c25f9a2eae90a3bd1ae0603f
RAMEES-KKVL <rameesp41750@gmail.com>
2025-07-14 22:35:42 +0530

    Completed UI design of spces of contact-us page

--- src/app/user-layout/pages/contact/contact.component.ts
+++ src/app/user-layout/pages/contact/contact.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from './components/banner/banner.component';
+import { SpacesComponent } from './components/spaces/spaces.component';
 
 @Component({
   selector: 'app-contact',
   imports: [
-    BannerComponent
+    BannerComponent,
+    SpacesComponent,
   ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
