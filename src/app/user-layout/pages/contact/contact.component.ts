e77db0c2ccb51011bfa794afff9b525bcf54c5cd
RAMEES-KKVL <rameesp41750@gmail.com>
2025-07-15 14:35:55 +0530

    Completed UI design of contact form

--- src/app/user-layout/pages/contact/contact.component.ts
+++ src/app/user-layout/pages/contact/contact.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from './components/banner/banner.component';
 import { SpacesComponent } from './components/spaces/spaces.component';
+import { ContactFormComponent } from "./components/contact-form/contact-form.component";
 
 @Component({
   selector: 'app-contact',
   imports: [
     BannerComponent,
     SpacesComponent,
-  ],
+    ContactFormComponent
+],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
