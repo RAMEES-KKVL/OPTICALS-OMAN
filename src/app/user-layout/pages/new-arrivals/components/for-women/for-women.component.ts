4fcb3747de150c67d603b99dca0d3825de2bd513
RAMEES-KKVL <rameesp41750@gmail.com>
2025-07-18 17:35:55 +0530

    Completed UI design of womens section in new arrivals

---
+++ src/app/user-layout/pages/new-arrivals/components/for-women/for-women.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ScrollerComponent } from "../../../../../shared/components/scroller/scroller.component";
+
+@Component({
+  selector: 'app-for-women',
+  imports: [ScrollerComponent],
+  templateUrl: './for-women.component.html',
+  styleUrl: './for-women.component.css'
+})
+export class ForWomenComponent {
+
+}
--- src/app/user-layout/pages/new-arrivals/new-arrivals.component.ts
+++ src/app/user-layout/pages/new-arrivals/new-arrivals.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from './components/banner/banner.component';
 import { ForKidsComponent } from "./components/for-kids/for-kids.component";
+import { ForWomenComponent } from "./components/for-women/for-women.component";
 
 @Component({
   selector: 'app-new-arrivals',
   imports: [
     BannerComponent,
-    ForKidsComponent
+    ForKidsComponent,
+    ForWomenComponent
 ],
   templateUrl: './new-arrivals.component.html',
   styleUrl: './new-arrivals.component.css'
