d8c7e8da3c62200b6dad29e810f4caf3f05665dc
RAMEES-KKVL <rameesp41750@gmail.com>
2025-07-21 17:21:04 +0530

    Completed UI design of featured product component of kids product page

---
+++ src/app/user-layout/pages/kids/components/featured-items/featured-items.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-featured-items',
+  imports: [
+    CommonModule
+  ],
+  templateUrl: './featured-items.component.html',
+  styleUrl: './featured-items.component.css'
+})
+export class FeaturedItemsComponent {
+  showImages = false; 
+
+  toggleImages(): void {
+    this.showImages = !this.showImages;
+  }
+}
--- src/app/user-layout/pages/kids/kids.component.ts
+++ src/app/user-layout/pages/kids/kids.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CategoryBannerComponent } from "../../../shared/components/category-banner/category-banner.component";
+import { FeaturedItemsComponent } from "./components/featured-items/featured-items.component";
 
 @Component({
   selector: 'app-kids',
-  imports: [CategoryBannerComponent],
+  imports: [CategoryBannerComponent, FeaturedItemsComponent],
   templateUrl: './kids.component.html',
   styleUrl: './kids.component.css'
 })
 export class KidsComponent {
 
-}
+} 
